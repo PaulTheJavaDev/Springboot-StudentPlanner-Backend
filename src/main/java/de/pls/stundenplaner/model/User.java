c7a541d5382b1fe97dd3b9fcc0358cd4fe314f55
Paul-Ludwig Simon <p-PSI@hup.de>
2025-12-18 13:19:42 +0100

    Successfuly migrated from H2 Database to MongoDB

--- src/main/java/de/pls/stundenplaner/model/User.java
+++
@@ -1,5 +0,0 @@
-package de.pls.stundenplaner.model;
-
-import java.util.UUID;
-
-public record User(String firstName, String lastName, UUID uuid) {}
---
+++ src/main/java/de/pls/stundenplaner/repository/MongoAssignmentRepository.java
@@ -0,0 +1,4 @@
+package de.pls.stundenplaner.repository;
+
+public class MongoAssignmentRepository {
+}
